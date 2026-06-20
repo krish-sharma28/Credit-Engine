962ca99cd1e35cdf4d6019ce4a77d247402b6b0e
Krish Sharma <krish@Krishs-MacBook-Pro-3.local>
2026-06-20 22:41:31 +0530

    Trained Linear Reg model

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
 
 df = pd.read_csv('data/nbfc_credit_data.csv')
 
 y = df['default']
 X = df.drop(columns=['default', 'nbfc'])
-
 X = pd.get_dummies(X, columns=['loan_type', 'employment_type'])
-print(X.columns.tolist())
-print(X.shape)
+
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, 
+                                                    random_state=28)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+lr_model = LogisticRegression(max_iter=1000)
+lr_model.fit(X_train, y_train)
+
+print("Model trained")
